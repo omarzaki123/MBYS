89fb2f96a7f37a723e3ee8309175cdea9dd38e18
omarzaki123 <omar.zaki@yale.edu>
2018-04-04 00:27:49 -0400

    Add files via upload

---
+++ Target Support Files/Pods-CompanyApp/Pods-CompanyApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CompanyApp : NSObject
+@end
+@implementation PodsDummy_Pods_CompanyApp
+@end
--- sharedDocsCell.swift
+++ sharedDocsCell.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+
+class sharedDocsCell: UITableViewCell{
+    
+    //If this variable is 0, then the start is NOT favorited
+    var isFavorited = 0;
+    
+    @IBOutlet weak var documentTitle: UILabel!
+    
+    @IBOutlet weak var sharedDate: UILabel!
+    
+    @IBOutlet weak var documentImage: UIImageView!
+
+    @IBAction func favoriteStarToggle(sender: AnyObject) {
+        
+        if(isFavorited == 0){
+            print("Its empty")
+            sender.setImage(UIImage(named: "icon favorite filled.png"), forState: .Normal)
+            isFavorited = 1
+        } else {
+            print("Or is it")
+            sender.setImage(UIImage(named: "icon favorite empty.png"), forState: .Normal)
+            isFavorited = 0
+        }
+    }
+
+}
